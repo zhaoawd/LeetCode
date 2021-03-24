0d7b8828d1edb65c091e0c34a3a67f4d4c434d16
zhaoawd <zhaoawd@gmail.com>
2021-03-24 13:59:16 +0800

    tree:PathSum:Add

---
+++ tree/PathSum.java
@@ -0,0 +1,22 @@
+package tree;
+
+public class PathSum {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (null == root) {
+            if (targetSum != 0) {
+                return false;
+            } else {
+                return false;
+            }
+        }
+        if (null == root.left && null == root.right) {
+            if (root.val == targetSum) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
