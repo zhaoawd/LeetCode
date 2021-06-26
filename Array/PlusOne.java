e5b25d4f68930814a789521ad967133d8b129e35
zhaoawd <zhaoawd@gmail.com>
2021-06-26 12:09:13 +0800

    Array:PlusOne:edit

--- Array/PlusOne.java
+++ Array/PlusOne.java
@@ -2,14 +2,22 @@ package Array;
 
 public class PlusOne {
 
-    public int[] plusOne(int[] digits){
-        int val = 0;
-        
-        val++;
-        String[] newVal = String.valueOf(val).split("");
-        digits = new int[newVal.length];
-        for (int i = 0; i < newVal.length; i++) {
-            digits[i] = Integer.parseInt(newVal[i]);
+    public int[] plusOne_1(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--) {
+            if(digits[i] != 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            } else if(i == 0) {
+                digits[i] = 0;
+                int[] plusResult = new int[digits.length+1];
+                plusResult[0] = 1;
+                for(int j = 1; j<plusResult.length-1 ;j++){
+                    plusResult[j] = digits[j-1];
+                }
+                return plusResult;
+            } else {
+                digits[i] = 0;
+            }
         }
         return digits;
     }
