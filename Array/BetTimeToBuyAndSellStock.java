8572dedc74ffb9cc7e9001afb0cbf9962ca981c3
zhaoawd <zhaoawd@gmail.com>
2021-07-11 12:33:15 +0800

    Array:BestTimeToByAndSellStock:Add

---
+++ Array/BetTimeToBuyAndSellStock.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class BetTimeToBuyAndSellStock {
+
+    public int maxProfitOnePass(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
+    public int maxProfitBruteForce(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+            }
+        }
+        return maxprofit;
+    }
+}
